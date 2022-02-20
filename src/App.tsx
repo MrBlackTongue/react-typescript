ad5617b144d1a6d10da45e8fbac2fbaa37abeadd
Svetlana Tyshkevich <Lana-14-92@mail.ru>
2022-02-20 10:14:34 +0300

    Седьмой урок. Типизация функций высшего порядка

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,40 @@
-import React, {Component} from 'react';
-import ReactDOM from "react-dom";
+import React, {useState} from 'react';
 
-// type PortalProps = {
-//     children: React.ReactNode
-// }
-//
-// class Portal extends Component <PortalProps> {
-//
-//     private el: HTMLDivElement = document.createElement('div')
-//
-//     public componentDidMount(): void {
-//         document.body.appendChild(this.el)
-//     }
-//
-//     public componentWillUnmount(): void {
-//         document.body.removeChild(this.el)
-//     }
-//
-//     public render(): React.ReactElement<PortalProps> {
-//         return ReactDOM.createPortal(this.props.children, this.el)
-//     }
-// }
-//
-// class MyComponent extends Component<{}, { count: number }> {
-//     state = {
-//         count: 0,
-//     }
-//     handleClick = () => {
-//         this.setState(({ count }) => ({
-//             count: ++count,
-//         }));
-//     }
-//     render() {
-//         return (
-//             <div onClick={this.handleClick}>
-//                 <h1>Clicks: {this.state.count}</h1>
-//                 <Portal>
-//                     <h2>TEST PORTAL</h2>
-//                     <button>Click</button>
-//                 </Portal>
-//             </div>
-//         );
-//     }
-// }
-
-interface IContext {
-    isAuth: Boolean,
-    toggleAuth: () => void
+type BaseProps = {
+    primTitle: string,
+    secTitle?: string,
 }
 
-const  AuthContext = React.createContext<IContext>({
-    isAuth: false,
-    toggleAuth: () => {},
-})
-
-class Login extends Component {
-    static contextType = AuthContext
-    context!: React.ContextType<typeof AuthContext>
-
-    render() {
-        const {toggleAuth, isAuth} = this.context
-
-        return (
-            <button onClick={toggleAuth}>
-                {!isAuth ? 'Login' : 'Logout'}
-            </button>
-        )
-    }
+type InjectedProps = {
+    toggleStatus: Boolean,
+    toggle: () => void,
 }
 
-const Profile: React.FC = (): React.ReactElement => (
-    <AuthContext.Consumer>
-        {({ isAuth }: IContext) => (
-            <h1>{!isAuth ? 'Please log in' : 'You are logged in'}</h1>
-            )}
-    </AuthContext.Consumer>
+const Button = ({primTitle, secTitle, toggle, toggleStatus}: any) => (
+    <button onClick={toggle}>
+        {toggleStatus ? primTitle : secTitle}
+    </button>
 )
 
-class Context extends Component<{}, { isAuth: Boolean}> {
-     readonly state = {
-        isAuth: false,
-    }
 
-    toggleAuth = () => {
-        this.setState(({ isAuth }) => ({
-            isAuth: !isAuth
-        }))
-    }
-
-    render() {
-        const {isAuth} = this.state
-        const context: IContext = {isAuth, toggleAuth: this.toggleAuth}
+const withToggle = <BaseProps extends InjectedProps>(PassedComponent: React.ComponentType<BaseProps>) => {
+    return (props: BaseProps) => {
+        const [toggleStatus, toggle] = useState(false)
 
         return (
-            <AuthContext.Provider value={context}>
-                <Login/>
-                <Profile/>
-            </AuthContext.Provider>
+            <PassedComponent
+                {...props as BaseProps}
+                toggle={() => toggle(!toggleStatus)}
+                toggleStatus={toggleStatus}
+            />
         )
     }
 }
 
+const ToogleButton = withToggle(Button)
 
-const App: React.FC = () => <Context/>
+
+const App: React.FC = () => <ToogleButton primTitle='Main Title' secTitle='Additional Title'/>
 
 export default App;
 
